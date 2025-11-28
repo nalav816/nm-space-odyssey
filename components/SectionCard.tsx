0859bb477a4aea8ad595879848e32d3d43cd8cef
Nadden <nalaventure123@gmail.com>
2025-11-27 21:11:34 -0500

    Changed Section Card header height

--- components/SectionCard.tsx
+++ components/SectionCard.tsx
@@ -1,7 +1,7 @@
 export default function SectionCard({ children, className, sectionName } : {children?: React.ReactNode, className?: string, sectionName?: string}){
     return (
         <div className = {"h-64 w-32 shadow-md section-card rounded-md border-blue-dark border-2 " + className}>
-            <div className ="w-full h-16 rounded-t bg-blue-darkest shadow-md p-4 flex items-center">
+            <div className ="w-full h-12 rounded-t bg-blue-darkest shadow-md p-4 flex items-center">
                 <div className = "text-xl text-glow-blue"> {sectionName} </div>
             </div>
             {children}
