2228306065610db4587ed85243f72b8456fe3a3b
Nadden <nalaventure123@gmail.com>
2025-11-30 14:23:45 -0500

    reorganized the layout a little bit. Will have to put this branch on hold until svg support is implemented because it seems that just using flat pngs won't allow us to implement a lot of planned features.

--- components/EmployeesQuarters.tsx
+++ components/EmployeesQuarters.tsx
@@ -28,8 +28,23 @@ export default function EmployeesQuarters({className} : {className?: string}){
 
     return (
         <SectionCard className = {"flex flex-col " + className} sectionName = "Employee's Quarters">
-            <div className ="flex-1 flex-col flex justify-end">
-                <div className = "h-36 w-full flex flex-col">
+            <div className ="flex-1 flex-col flex justify-between">
+                <div className="h-12 w-full flex p-4">
+                    <div className = "basis-1/4 flex gap-2 items-center">
+                        <img className="h-4 w-4 image-pixelated" src="/sprites/star.png"/>
+                        <div className="text-sm"> 0 / {ROOM_SIZE} </div>
+                    </div>
+                    <div className = "basis-2/4 gap-4 flex justify-center items-center">
+                        <button> &lt; </button>
+                        <div className="text-sm"> Room 1 / 2 </div>
+                        <button> &gt; </button>
+                    </div>
+                    <div className = "basis-1/4 flex gap-2 justify-end ">
+                        <img className="h-4 w-4 image-pixelated" src="/sprites/star.png"/>
+                        <div className="text-sm"> Rooms </div>
+                    </div>
+                </div>
+                <div className = "h-24 w-full flex flex-col">
                     <div className="flex justify-center">
                         {employees.map((employee, i) => (
                             <img key={i} className="h-16 w-16 image-pixelated" src={employee.modelUrl}></img>
@@ -37,11 +52,6 @@ export default function EmployeesQuarters({className} : {className?: string}){
 
                     </div>
                     <div className="bg-repeat bg-[url('/sprites/floorTile.png')] w-full h-8 bg-size-[32px_32px] image-pixelated"></div>
-                    <div className="flex justify-center relative gap-4">
-                        <button> &lt; </button>
-                        <div>  1 </div>
-                        <button> &gt; </button>
-                    </div>
                 </div>
                 
             </div>
