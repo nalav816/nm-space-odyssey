a48cfdab6b343cd77641d5b3849b5b7ce2ff9280
Nadden <nalaventure123@gmail.com>
2025-12-22 22:10:18 -0500

    Created the color icon component

---
+++ components/ColoredIcon.tsx
@@ -0,0 +1,14 @@
+export function ColoredIcon({iconUrl, className} : {iconUrl:string, className?:string}) {
+    return (
+        <div className = {`${className}`} style={
+            {
+                maskImage: `url(${iconUrl})`,
+                maskRepeat: "no-repeat",
+                maskSize: "contain",
+                WebkitMaskImage: `url(${iconUrl})`,
+                WebkitMaskRepeat: 'no-repeat',
+                WebkitMaskSize: "contain"
+            } 
+        }/>
+    )
+}
