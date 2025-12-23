f9d314db59789f2e96043532f5e82961080cdadb
Nadden <nalaventure123@gmail.com>
2025-12-23 01:51:33 -0500

    spacing and format changes

--- components/Launchpad.tsx
+++ components/Launchpad.tsx
@@ -31,36 +31,30 @@ const RocketPlatform = ({ rocketSprite }: { rocketSprite: string }) => {
 
     return (
         <div className="flex flex-col items-end relative">
-
-
-            
-
             {new Array(minYTileToFill).fill(0).map((_, i) => (
-                 i%3 == 0 && i != 0 ? (
-                        <div className="flex" key={i}>
-                            {new Array(minXTileToFill/2 - 1).fill(0).map((_, i) => (
-                                <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBridgeTile.png"/>
-                            ))}
-                            <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBridgeRightTile.png"/>
-                            <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformVertTile.png"/>
-                        </div>
-                    ) : (
-                         <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformVertTile.png" key={i}/>
-                    )
-                
-               
+                i % 3 == 0 && i != 0 ? (
+                    <div className="flex" key={i}>
+                        {new Array(minXTileToFill / 2 - 1).fill(0).map((_, i) => (
+                            <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBridgeTile.png" />
+                        ))}
+                        <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBridgeRightTile.png" />
+                        <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformVertTile.png" />
+                    </div>
+                ) : (
+                    <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformVertTile.png" key={i} />
+                )
             ))}
 
-            <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBottomVertTile.png"/>
+            <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformBottomVertTile.png" />
 
             <div className="flex">
-                <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformLeftTile.png"/>
-                {new Array(minXTileToFill-1).fill(0).map((_, i) => 
-                    (
-                        <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformTile.png" key={i}/>
-                    )
+                <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformLeftTile.png" />
+                {new Array(minXTileToFill - 1).fill(0).map((_, i) =>
+                (
+                    <ColoredSprite className="h-8 w-8 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformTile.png" key={i} />
+                )
                 )}
-                <ColoredSprite className="h-8 w-16 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformRightTile.png"/>
+                <ColoredSprite className="h-8 w-16 bg-blue-darker image-pixelated" spriteUrl="/sprites/platformRightTile.png" />
             </div>
 
             <img className={`absolute bottom-8 right-1/2 translate-x-1/2 image-pixelated`} src={rocketSprite} style={
@@ -68,16 +62,12 @@ const RocketPlatform = ({ rocketSprite }: { rocketSprite: string }) => {
                     height: `${rocketSize.height}px`,
                     width: `${rocketSize.width}px`
                 }
-            }/>
-
-
+            } />
         </div>
     )
 }
 
 export default function Launchpad({ className }: { className?: string }) {
-    const PLATFORM_COUNT = 1;
-
     return (
         <SectionCard iconUrl={"/sprites/launchpadIcon.png"} className={"flex flex-col " + className} sectionName="Launchpad">
             <div className="flex-1 min-h-0 flex-col flex justify-between">
@@ -85,11 +75,11 @@ export default function Launchpad({ className }: { className?: string }) {
                 <div className="flex-1 min-h-0 w-full flex flex-col items-center">
                     <div className="flex-1 w-full min-h-0 px-4">
                         <div className="h-full flex min-h-0 flex-col-reverse items-center overflow-auto w-full scrollbar-custom ">
-                            <RocketPlatform rocketSprite="/sprites/bigRed.png"/>
-                       
+                            <RocketPlatform rocketSprite="/sprites/bigRed.png" />
+
                         </div>
                     </div>
-                    
+
                     <TiledSprite tileUrl={"/sprites/grassTile.png"} className="bg-blue-darker w-full h-8 image-pixelated"></TiledSprite>
                 </div>
 
