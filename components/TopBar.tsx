6f1d106d2bb7c2779dfd6828918ec30705562538
Nadden <nalaventure123@gmail.com>
2025-12-23 13:12:44 -0500

    added ship wing icon

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,13 +1,15 @@
+import ColoredSprite from "./ColoredSprite";
+
 export default function TopBar({ items = 0, itemCapacity = 0, isLaunchpad = false } : {items?:number, itemCapacity?:number, isLaunchpad?:boolean}) {
     const roomName = isLaunchpad ? "Plot" : "Room"
     const roomCount = 2
     const currRoom = 1
-    const itemIconUrl = isLaunchpad ? "/sprites/placeholder.png" : "/sprites/employeeIcon.png";
+    const itemIconUrl = isLaunchpad ? "/sprites/shipWingIcon.png" : "/sprites/employeeIcon.png";
 
     return (
         <div className="w-full flex justify-between py-2 px-4 items-center">
             <div className="flex w-16 gap-2">
-                <img className="h-4 w-4 image-pixelated" src={itemIconUrl}/>
+                <ColoredSprite className="bg-blue-lightest h-4 w-4 image-pixelated" spriteUrl={itemIconUrl}/>
                 <div className="text-sm"> {items} / {itemCapacity} </div>
             </div>
             <div className="basis-2/4 gap-4 flex justify-center">
@@ -17,7 +19,7 @@ export default function TopBar({ items = 0, itemCapacity = 0, isLaunchpad = fals
             </div>
             <div className="flex justify-end gap-2 w-16 items-center">
                 <button className="h-4 w-4 bg-blue rounded-md shadow hover:cursor-pointer hover:bg-blue-light transition duration-200 ease-in-out">
-                    <img className="h-full w-full image-pixelated" src="/sprites/plusIcon.png" />
+                    <ColoredSprite className="bg-blue-lightest h-full w-full image-pixelated" spriteUrl="/sprites/plusIcon.png" />
                 </button>
 
                 <div className="text-sm"> {roomName}s </div>
