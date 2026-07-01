d018ef4a1ae4de6ea3ce4ff21c929f1918076f77
Nadden <nalaventure123@gmail.com>
2026-07-01 10:17:33 -0400

    changed section card component and added texture images

--- components/SectionCard.tsx
+++ components/SectionCard.tsx
@@ -1,9 +1,15 @@
 export default function SectionCard({ children, className, sectionName, iconUrl = "/sprites/placeholder.png" } : {children?: React.ReactNode, className?: string, sectionName?: string, iconUrl?: string}){
     return (
-        <div className = {"h-64 w-32 shadow-md section-card rounded-md border-blue-dark border-2 border-dashed " + className}>
-            <div className ="w-full h-12 rounded-t bg-blue-darkest shadow-md p-4 gap-2 flex items-center">
-                <img className="h-8 w-8 image-pixelated" src = {iconUrl} />
-                <div className = "text-lg text-glow-blue"> {sectionName} </div>
+        <div className = {"relative h-64 w-32 shadow-md bg-blue rounded-md z-10 " + className}>
+            <div className = "rounded-md texture metallic-texture opacity-60 z-20" />
+            <div className ="z-30 relative w-full h-12 rounded-t bg-blue-dark shadow-md p-4 gap-2 flex items-center">
+                <div className = "texture geometric-texture opacity-10" />
+                {
+                    /* 
+                    <img className="h-8 w-8 image-pixelated" src = {iconUrl} />
+                    */
+                }
+                <div className = "text-md"> {sectionName} </div>
             </div>
             {children}
         </div>
