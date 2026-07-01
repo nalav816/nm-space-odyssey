c78fcf208483abafba0dfc198f7c617d79ca179f
Nadden <nalaventure123@gmail.com>
2026-07-01 10:50:23 -0400

    changed background, added images

--- components/Game.tsx
+++ components/Game.tsx
@@ -11,18 +11,19 @@ export default function Game({ playerData }: { playerData:Player }) {
     const [player, setPlayer] = useState<Player>(playerData);
     
     return (
-        <div className="bg-blue-darkest w-screen h-screen flex justify-center relative text-white font-jaro">
-            <img className="h-screen opacity-25 blur-xs w-screen object-cover absolute z-0" src="/imgs/starBackground.jpg"></img>
-            <div className="px-12 py-8 flex gap-8 h-screen w-screen max-w-7xl absolute z-10">
+        <div className="relative bg-radial-gradient w-screen h-screen flex justify-center relative text-white font-jaro">
+            <div className= "texture opacity-2 z-10"/>
+            <img className="h-screen opacity-10 blur-[2px] w-screen object-cover absolute z-0" src="/imgs/space2.jpg"></img>
+            <div className="px-12 py-8 flex gap-8 h-screen w-screen max-w-7xl absolute z-20">
                 <div className="h-full basis-1/3 flex flex-col">
                     <div className="flex flex-col gap-4 pb-24">
                         <div className="text-5xl title-glow">
                             <span className="text-2xl text-blue-light"> n&m </span>
                             Space <br /> <span className="text-blue-lightest"> Odyssey </span> </div>
-                        <div className="text-blue-lightest text-xl"> {player.username}'s Space Station </div>
+                        <div className="text-blue-lightest text-lg"> {player.username}'s Space Station </div>
                         <div className="flex gap-4 items-center">
-                            <img className="h-8 w-8 image-pixelated" src="/sprites/cash.png" />
-                            <div className=" text-green text-glow-green text-xl"> ${player.netWorth} </div>
+                            <img className="h-8 w-8 image-pixelated image-glow-green" src="/sprites/cash.png" />
+                            <div className=" text-green text-glow-green text-lg"> ${player.netWorth} </div>
                         </div>
 
                     </div>
