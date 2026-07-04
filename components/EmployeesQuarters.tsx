a79a94da1a9c190e086ea8465874632e051cdd89
Nadden <nalaventure123@gmail.com>
2026-07-04 17:45:06 -0400

    started working on room selection menu

--- components/EmployeesQuarters.tsx
+++ components/EmployeesQuarters.tsx
@@ -1,14 +1,14 @@
 "use client"
-import { PlayerContext } from "./Game"
-import { useState, useEffect, useContext } from "react"
 import SectionCard from "./SectionCard"
 import TiledSprite from "./TiledSprite"
 import TopBar from "./TopBar"
 import { Astronaut } from "./Astronaut"
-import ColoredSprite from "./ColoredSprite"
 import TintedSprite from "./TintedSprite"
 import type { Player } from "@/views/player"
 import { usePlayer } from "@/hooks/usePlayer"
+import { useState } from "react"
+import { ArrowLeft } from "lucide-react"
+import { ArrowRight } from "lucide-react"
 
 export function getNextAvailableQuartersSlot(player: Player) {
     let slot = 1
@@ -42,31 +42,58 @@ export function getNextAvailableQuartersSlot(player: Player) {
 
 export default function EmployeesQuarters({ className }: { className?: string }) {
     const [player, setPlayer] = usePlayer();
-    const ROOM_SIZE = 5
-    const currRoom = 1
-    const roomCount = 2
+    const [room, setRoom] = useState(1);
+
+    const handleArrowClicked = ( isRightArrow: boolean ) => {
+        if (isRightArrow) {
+            setRoom(prev => prev + 1)
+        } else {
+            setRoom (prev => prev - 1)
+        }
+    }
 
     return (
         <SectionCard iconUrl={"/sprites/astronautQuartersIcon.png"} className={"flex flex-col " + className} sectionName="Astronaut's Quarters">
             <div className="m-2 border-2 border-dashed border-blue-dark rounded relative z-20 flex-1 flex-col flex justify-between card-radial-gradient">
-                <div className="z-20 texture opacity-5" />
-                <TopBar items={player.astronauts.length} itemCapacity={ROOM_SIZE} />
-                <div className="flex flex-col justify-end h-full w-full">
-                    <div className="px-4 flex items-end justify-center w-full h-full">
-                        {new Array(ROOM_SIZE).fill(0).map((_, i) => {
+                <div className="z-10 texture opacity-5" />
+                <TopBar
+                    items={player.astronauts.length} 
+                    itemCapacity={player.roomSpaceCap}
+                    currRoom={room}
+                    roomCount={player.astronautRoomCount}
+                />
+                <div className="flex flex-col justify-end flex-1 w-full overflow-hidden">
+                    <div className="px-4 flex items-end justify-center flex-1 w-full min-h-0">
+                        {new Array(player.roomSpaceCap).fill(0).map((_, i) => {
                             const astronaut = player.astronauts.find(a => a.occupiedSlot == i + 1)
                             return (
-                                <div key={i} className="basis-1/5 h-full min-w-0 flex items-end justify-center">
+                                <div key={i} className={`relative basis-1/5 h-full min-w-0 flex items-end justify-center`}>
                                     {astronaut ? (
                                         <Astronaut astronautData={astronaut} />
                                     ) : (
-                                        <TintedSprite tintIntensity={1} spriteUrl="/sprites/scrub.png" className="text-blue-darker image-pixelated" />
+                                        <TintedSprite tintIntensity={1} spriteUrl="/sprites/scrub.png" className="relative z-10 text-blue-darker image-pixelated" />
                                     )}
                                 </div>
                             )
                         })}
                     </div>
                     <TiledSprite className="w-full h-8 bg-blue-dark image-pixelated" tileUrl="/sprites/floortile.png" />
+                    <div className="w-full px-4 pb-2 flex justify-between">
+                        <button
+                            onClick={() => handleArrowClicked(false)}
+                            disabled = {room <= 1}
+                            className= "disabled:opacity-0 disabled:pointer-events-none"
+                        >
+                            <ArrowLeft className="transition-transform duration-200 hover:-translate-y-1 hover:text-blue-lightest text-blue-light h-6 w-6" />
+                        </button>
+                        <button
+                            onClick={() => handleArrowClicked(true)}
+                            disabled = {room >= player.astronautRoomCount}
+                            className= "disabled:opacity-0 disabled:pointer-events-none"
+                        >
+                            <ArrowRight className="transition-transform duration-200 hover:-translate-y-1 hover:text-blue-lightest text-blue-light h-6 w-6" />
+                        </button>
+                    </div>
                 </div>
 
             </div>
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -6,20 +6,34 @@ const RoomButton = ({ number, isActive }: { number: number, isActive: boolean })
         border-[1px] border-dashed text-sm text-white h-8 w-8 rounded
         ${isActive ? "bg-blue-light border-blue" : "bg-blue border-blue-dark"}`
         }>
-            <div className="w-full h-full texture geometric-texture opacity-10"/>
+            <div className="w-full h-full texture geometric-texture opacity-10" />
             {number}
         </button>
     )
 }
 
-export default function TopBar({ items = 0, itemCapacity = 0, isLaunchpad = false }: { items?: number, itemCapacity?: number, isLaunchpad?: boolean }) {
+export default function TopBar(
+    {
+        items = 0,
+        itemCapacity = 0,
+        currRoom = 1,
+        roomCount = 0,
+        isLaunchpad = false
+    }
+        :
+    {
+        items?: number,
+        itemCapacity?: number,
+        currRoom?: number,
+        roomCount?: number,
+        isLaunchpad?: boolean
+    }
+) {
     const roomName = isLaunchpad ? "Plot" : "Room"
-    const roomCount = 25
-    const currRoom = 1
     const itemIconUrl = isLaunchpad ? "/sprites/shipIcon.png" : "/sprites/astronautIcon.png";
 
     return (
-        <div className="px-4 py-2 w-full flex flex-col min-h-0 max-h-22">
+        <div className="relative z-40 px-4 py-2 w-full flex flex-col">
             <div className="gap-4 flex items-center">
                 <div className="text-md"> {roomName + " " + currRoom}  </div>
                 <div className="flex gap-1">
@@ -28,9 +42,9 @@ export default function TopBar({ items = 0, itemCapacity = 0, isLaunchpad = fals
                 </div>
             </div>
 
-            <div className="flex flex-wrap mt-2 pb-2 gap-2 min-h-0 overflow-y-scroll scrollbar-custom border-b-2 border-blue-dark">
+            <div className="flex flex-wrap mt-2 pb-2 gap-2 min-h-0 max-h-10 overflow-y-scroll scrollbar-custom ">
                 {Array.from({ length: roomCount }).map((_, i) => (
-                    <RoomButton key={i} number={i + 1} isActive={i+1 == currRoom} />
+                    <RoomButton key={i} number={i + 1} isActive={i + 1 == currRoom} />
                 ))}
             </div>
         </div>
